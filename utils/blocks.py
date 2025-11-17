1c2b63eba9e37bfb2e4829ccc514fbf1705d37dd
garMKS228 <ptlir163@gmail.com>
2025-11-17 19:56:35 +0300

    Update blocks.py

--- utils/blocks.py
+++ utils/blocks.py
@@ -19,7 +19,16 @@ def process_block_return(block_text: str) -> dict:
     """
     analyzer = LayoutAnalyzer()
     analyzer.analyze_text(block_text)
-    return analyzer.reverser  # словарь с нагрузкой для всех раскладок
+
+    # Добавляем анализ последовательностей
+    sequence_stats = analyzer.analyze_sequences(block_text)
+
+    result = {
+        'finger_data': analyzer.reverser,  # существующие данные
+        'sequence_stats': sequence_stats  # новые данные по последовательностям
+    }
+
+    return result
 
 def merge_block_data(main_analyzer: LayoutAnalyzer, block_data: dict):
     """
@@ -31,7 +40,7 @@ def merge_block_data(main_analyzer: LayoutAnalyzer, block_data: dict):
 
     ВЫХОД: Нет (данные добавляются в основной анализатор)
     """
-    for layout_name, vals in block_data.items():
+    for layout_name, vals in block_data['finger_data'].items():
         layout = main_analyzer.layouts[layout_name]
 
         # Суммируем нагрузку по пальцам
@@ -47,8 +56,7 @@ def merge_block_data(main_analyzer: LayoutAnalyzer, block_data: dict):
         layout.counter_fingers['f4r'] += vals['right'][3]
         layout.counter_fingers['f5r'] += vals['right'][4]
 
-        # Суммируем переходы между руками
-        layout.hand_changes += vals['two_handed']
+
 
         layout.key_presses['f1l'] += vals['left_press'][0]
         layout.key_presses['f2l'] += vals['left_press'][1]
@@ -61,3 +69,25 @@ def merge_block_data(main_analyzer: LayoutAnalyzer, block_data: dict):
         layout.key_presses['f3r'] += vals['right_press'][2]
         layout.key_presses['f4r'] += vals['right_press'][3]
         layout.key_presses['f5r'] += vals['right_press'][4]
+
+
+
+
+        # Новое: объединение статистики последовательностей
+    if not hasattr(main_analyzer, 'sequence_stats_accumulated'):
+        main_analyzer.sequence_stats_accumulated = {}
+
+    for layout_name, stats in block_data['sequence_stats'].items():
+        if layout_name not in main_analyzer.sequence_stats_accumulated:
+            main_analyzer.sequence_stats_accumulated[layout_name] = {
+                'udp_2gram': 0, 'chudp_2gram': 0, 'nudp_2gram': 0,
+                'udp_3gram': 0, 'chudp_3gram': 0, 'nudp_3gram': 0,
+                'udp_4gram': 0, 'chudp_4gram': 0, 'nudp_4gram': 0,
+                'one_handed_2gram': 0, 'one_handed_3gram': 0, 'one_handed_4gram': 0,
+                'two_handed_2gram': 0, 'two_handed_3gram': 0, 'two_handed_4gram': 0,
+                'total_sequences': 0
+            }
+
+        # Суммируем статистику последовательностей
+        for key in main_analyzer.sequence_stats_accumulated[layout_name]:
+            main_analyzer.sequence_stats_accumulated[layout_name][key] += stats[key]
